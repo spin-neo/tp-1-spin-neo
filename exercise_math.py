aaa99ab9fe5045dec6c21c552133ac6070b3947c
spin-neo <mpfogliato@mail.austral.edu.ar>
2025-03-23 22:50:11 -0300

    tarea Fogliato Maria Paz

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,36 @@
 def math():
-    a = 57
-    b = 7
+a= 55
+b= 66
+'SUMA Y RESTA (+ Y -)'
+
+suma = a + b     #RTA= 121
+
+resta = a - b    #RTA= -11
+
+'MULTI Y DIVISION ( * y / )'
+
+multi = a * b     #RTA=3630
+
+division = a / b #devuelve un dato FLOAT    ---SIEMPRE      #RTA= 0.8333333333333334
+
+#potenciación (exponente **)
+
+exponente = a ** b        #RTA= 7310338828254284348684765759307822731264010134569603785204110402552282244915959896758295144536532461643218994140625
+
+#division baja ( // ) "DEVUELVE UN ENTERO REDONDEADO HACÍA ABAJO; LO QUE ESTA AL LADO DE LA COMA LO BORRA"
+
+division_baja = a // b    #RTA= 0
+
+'RESTO o MÓDULO ( % )' 
+
+resto = a % b   #doce/5 nos da un resto = 2 lo que sobra sin dividir x 5   #RTA= 55
+
+#--------------------------------------- correr el programa y ver respuestas en la terminal
+
+print(suma)
+print(resta)
+print(multi)
+print(division)
+print(exponente)
+print(division_baja)
+print(resto)
