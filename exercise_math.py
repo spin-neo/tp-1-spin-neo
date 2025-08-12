8a03a55bd85a546bfac6ef7ee284962d38f5dca4
spin-neo <mpfogliato@mail.austral.edu.ar>
2025-08-12 20:23:12 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    resta = a - b
+    producto = a * b
+    promedio = (a + b) / 2
+    cociente_entero = a // b
+    resto = a % b
+    division_real = a / b
+    print(suma)               # 64
+    print(resta)              # 50
+    print(producto)           # 399
+    print(promedio)           # 32.0
+    print(cociente_entero)    # 8
+    print(resto)              # 1
+    print(division_real)      # 8.142857142857142
